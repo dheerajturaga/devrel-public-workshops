306b37f41f44edce9da76216faed5a9b4fd9774d
dheerajturaga <dheerajturaga@gmail.com>
2024-09-11 23:49:13 +0000

    Fix meaning of life

--- dags/exercises/top_level_code.py
+++ dags/exercises/top_level_code.py
@@ -8,14 +8,19 @@ from include.helper_functions import expensive_api_call
 @dag(start_date=datetime(2023, 1, 1), max_active_runs=3, schedule=None, catchup=False, tags=["exercise"],)
 def bad_practices_dag_1():
 
-    the_meaning_of_life_the_universe_and_everything = expensive_api_call()
+    #the_meaning_of_life_the_universe_and_everything = expensive_api_call()
+    
+
+    @task
+    def the_meaning_of_life_the_universe_and_everything():
+        return expensive_api_call()
 
     @task
     def reveal_the_meaning_of_life_the_universe_and_everything(the_answer):
         print(f"The meaning of life, the universe, and everything is... {the_answer}.")
 
     reveal_the_meaning_of_life_the_universe_and_everything(
-        the_meaning_of_life_the_universe_and_everything
+        the_meaning_of_life_the_universe_and_everything()
     )
 
 
